579a782fe458c932230019734efa3b5b8fe160d7
Mahmud <mhoss017@uOttawa.ca>
2025-01-09 21:18:31 -0500

    Fix database initialization logic with app context revised

--- app.py
+++ app.py
@@ -159,8 +159,3 @@ def plot_png():
     plt.savefig(img, format='png')
     img.seek(0)
     return Response(img.getvalue(), mimetype='image/png')
-
-if __name__ == "__main__":
-    # Get the port from the environment variable or default to 5000
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
